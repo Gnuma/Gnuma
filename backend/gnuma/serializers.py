6cdebb032fd699b8dce52f6a3602a8e7b4ddf751
ihategoto <cdemilio80@gmail.com>
2019-01-12 17:42:05 +0100

    OfficeSerialzier, ClassSerializer, BookSerializer have been defined.

--- backend/gnuma/serializers.py
+++ backend/gnuma/serializers.py
@@ -3,8 +3,22 @@ from .models import GnumaUser, Book, Office, Class, Ad
 from django.contrib.auth.models import User
 
 
+class OfficeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Office
+        fields = '__all__'
+
+class ClassSerializer(serializers.ModelSerializer):
+    office = OfficeSerializer(many = False, read_only = False)
+
+    class Meta:
+        model = Class
+        fields = '__all__'
+
+
 
 class BookSerializer(serializers.ModelSerializer):
+    classes = ClassSerializer(many = True, read_only = True)
     class Meta:
         model = Book
-        fields = '__all__'
+        fields = ('isbn', 'title', 'author', 'classes')
