dc440ee6ec2a0dbb60c3adec56d086f61e953934
ihategoto <cdemilio80@gmail.com>
2019-01-12 17:41:34 +0100

    Book models added to admin page.

--- backend/gnuma/admin.py
+++ backend/gnuma/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import  GnumaUser, Office, Class, Ad
+from .models import  GnumaUser, Office, Class, Ad, Book
 
 admin.site.register(Ad)
 admin.site.register(Class)
 admin.site.register(Office)
 admin.site.register(GnumaUser)
+admin.site.register(Book)
