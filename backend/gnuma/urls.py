a56e9c82d2713cd507f40fd15b34d6cdcf3c6b59
ihategoto <cdemilio80@gmail.com>
2019-01-12 17:37:38 +0100

    Added: router for the book management viewset

--- backend/gnuma/urls.py
+++ backend/gnuma/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path, include
 from .views import init_user
+from rest_framework import routers
+from .views import BookManager
 
+router = routers.SimpleRouter()
+router.register(r'books', BookManager, basename = "book")
 
 urlpatterns = [
     path('init/', init_user),
 ]
+
+urlpatterns += router.urls
