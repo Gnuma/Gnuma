35ccbcd5ce680263b0661c0fb170fc6bf5835eec
ihategoto <cdemilio80@gmail.com>
2019-02-13 11:46:02 +0100

    IMAGES_DIR & IMAGES_URL added

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -15,6 +15,10 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Directory containing ads' images.
+IMAGES_DIR = os.path.join(BASE_DIR, 'img/')  
+IMAGES_URL = 'img/'    
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
